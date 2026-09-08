b25b2830dadaca64bbfbc74486d7024e24d65de4
Bach Nguyen <btn9@duke.edu>
2026-09-08 20:58:28 +0800

    [SET-UP]-Socket Set-up

--- src/main/java/com/counterfactual/httpserver/HTTPServer.java
+++ src/main/java/com/counterfactual/httpserver/HTTPServer.java
@@ -2,6 +2,11 @@ package com.counterfactual.httpserver;
 
 import com.counterfactual.httpserver.config.Configuration;
 import com.counterfactual.httpserver.config.ConfigurationManager;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HTTPServer {
   public static void main(String[] args){
@@ -12,5 +17,33 @@ public class HTTPServer {
 
     System.out.println("Port:" + config.getPort());
     System.out.println("Webroot:" + config.getWebroot());
+
+    try {
+      ServerSocket serverSocket = new ServerSocket(config.getPort());
+      Socket socket = serverSocket.accept();
+
+      InputStream inputStream = socket.getInputStream();
+      OutputStream outputStream = socket.getOutputStream();
+
+      String html = "<html><head><title>HelloWorld</title></head><body><h1>My server</h1></body></html>";
+
+      final String CRLF = "\n\r"; //13, 10
+
+      String response =
+          "HTTP/1.1 200 OK" + CRLF +  //Status Line : HTML Version Respond_Code Respond_message
+          "Content-Length: " + html.getBytes().length + CRLF +
+            CRLF +
+            html +
+            CRLF + CRLF;
+
+      outputStream.write(response.getBytes());
+
+      inputStream.close();
+      outputStream.close();
+      socket.close();
+      serverSocket.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
